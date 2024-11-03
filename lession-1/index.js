c4f0f07e39f3593646b1b125512d1be709d0053d
mahesh-kumar-putta <puttamahesh27@gmail.com>
2024-11-03 17:49:13 +0530

    first lession

---
+++ lession-1/index.js
@@ -0,0 +1,7 @@
+console.log(`Hello world`);
+
+console.log(`How are you doing`);
+
+window.alert(`hey bro, how're you doing`);
+
+document.getElementById("msg").textContent = `This is dynamic content bro!`;
