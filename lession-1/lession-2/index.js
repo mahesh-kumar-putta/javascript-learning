bd45e376ed6b20084178ab98ca37cf1ea08640ed
mahesh-kumar-putta <puttamahesh27@gmail.com>
2024-11-03 18:35:48 +0530

    second lession-variables

---
+++ lession-1/lession-2/index.js
@@ -0,0 +1,25 @@
+// let x;
+// x=123;
+
+let age = 25;
+console.log(age);
+
+let price = 10.99;
+console.log(price);
+
+let gpa = 9.5;
+console.log(`you are ${age} years old and you paid ${price} lacs to get ${gpa} GPA`);
+
+console.log(typeof gpa);
+
+let name = `mahesh`;
+console.log(typeof name);
+
+let locationn = "Earth";
+
+document.getElementById("p1").textContent = `${name}`;
+
+document.getElementById("p2").textContent = `${age}`;
+
+document.getElementById("p3").textContent = `${locationn}`;
+
