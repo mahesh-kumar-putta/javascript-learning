246b5453d0e4d27de61d5290e262fa86047c0567
mahesh-kumar-putta <puttamahesh27@gmail.com>
2024-11-05 20:00:35 +0530

    fifth lession

---
+++ lession-1/lession-5/index.js
@@ -0,0 +1,56 @@
+
+// let age = window.prompt(`Enter your age`);
+
+// console.log(age+1, typeof age); //251, string
+
+// age = Number(age);
+
+// console.log(age+1, typeof age); //26, number
+
+// let x = "pizza";
+// let y = "pizza";
+// let z = "pizza";
+
+// x = Number(x);
+// y = String(y);
+// z = Boolean(z);
+
+// console.log(x, typeof x); //NaN 'number'
+// console.log(y, typeof y); // pizza string
+// console.log(z, typeof z); // true 'boolean'
+
+// let x = "0";
+// let y = "0";
+// let z = "0";
+
+// x = Number(x);
+// y = String(y);
+// z = Boolean(z);
+
+// console.log(x, typeof x); //0 'number'
+// console.log(y, typeof y); // 0 string
+// console.log(z, typeof z); // true 'boolean'
+
+// let x = "";
+// let y = "";
+// let z = "";
+
+// x = Number(x);
+// y = String(y);
+// z = Boolean(z);
+
+// console.log(x, typeof x); //0 'number'
+// console.log(y, typeof y); // "" string
+// console.log(z, typeof z); // false 'boolean'
+
+let x ;
+let y ;
+let z ;
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x); //NaN 'number'
+console.log(y, typeof y); // undefined string
+console.log(z, typeof z); // false 'boolean'
