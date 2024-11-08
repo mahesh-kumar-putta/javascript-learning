ee25e1cdc203ac84ba1f679c3db71bdee2d0d324
mahesh-kumar-putta <puttamahesh27@gmail.com>
2024-11-08 09:07:31 +0530

    Thirteeenth Lession

---
+++ lession-1/lession-13/index.js
@@ -0,0 +1,18 @@
+// Switch statement : can be an efficient replacement to many else if statements 
+
+
+let day = "pizza";
+
+switch(day){
+  case 1:
+    console.log(`Sunday`);
+    break;
+  case 2:
+    console.log(`Monday`);
+    break;
+  case 3:
+    console.log(`Tuesday`);
+    break;
+  default:
+    console.log(`No match found for ${day}`)
+}
