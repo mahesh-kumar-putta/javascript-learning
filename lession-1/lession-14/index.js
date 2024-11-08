8dfc101f3df336e0797c18ae770245a42c190083
mahesh-kumar-putta <puttamahesh27@gmail.com>
2024-11-08 09:20:05 +0530

    Fourteenth Lession

---
+++ lession-1/lession-14/index.js
@@ -0,0 +1,23 @@
+
+let userName = "Mahesh-Kumar-Putta     ";
+
+console.log(userName.charAt(0));
+
+//first index of the given char
+console.log(userName.indexOf("a"));
+
+//last index of the given char
+console.log(userName.lastIndexOf("a"));
+
+
+console.log(userName.length);
+
+// removes all whitespaces
+userName = userName.trim();
+
+console.log(userName)
+
+console.log(userName.toLocaleLowerCase());
+
+console.log(userName.replaceAll("-","*"));
+
