7d4643c9e30dde26ff6bb8dfd8932a4c76ed9ad3
mahesh-kumar-putta <puttamahesh27@gmail.com>
2024-11-08 09:01:28 +0530

    Twelth lession

---
+++ lession-1/lession-12/index.js
@@ -0,0 +1,8 @@
+// Ternary Operator : A shortcut to if and else statements helps to assign a variable based on a condition 
+//     condition ? CodeIfTrue : CodeIfFalse
+
+let age = 27
+
+let message = age>25?`you are adult`:`you are kid`;
+
+console.log(message);
