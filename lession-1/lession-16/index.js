237f4eaefe30bf5e965b6511fc1ba39bb954deb7
mahesh-kumar-putta <puttamahesh27@gmail.com>
2024-11-09 11:06:59 +0530

    Sixteenth Lession

---
+++ lession-1/lession-16/index.js
@@ -0,0 +1,11 @@
+// Method chaining : calling one method from another in a single statement
+// 
+
+const userName = window.prompt(`Enter your name`);
+
+console.log(userName);
+
+console.log(userName.trim().toUpperCase());
+
+
+
