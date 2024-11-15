602292ca14c0eb767aee09b50f8713c6f829625c
mahesh-kumar-putta <puttamahesh27@gmail.com>
2024-11-15 22:57:12 +0530

    Arrow function

--- lession-1/lession-35/index.js
+++ lession-1/lession-35/index.js
@@ -1,30 +1,8 @@
-// function declaration : a resuable block of code that performs a specified task
-// function expression : a way to define a function as value or variable
+// Arraow function : A concise way to write a function expression for simple functions that you use only once
 
-function hello(){
-  console.log(`Hello world!`);
+const hello = function(){
+  console.loh(`hell`);
 }
 
-const helloworld = function(){
-  console.log(`Hello world from expression`);
-}
-
-hello();
-helloworld();
-
-setTimeout(helloworld, 3000);
-
-const nums = [1,2,3,4,5,6];
-
-const squarefun = function(element){
-  return element*element;
-}
-
-const square = nums.map(squarefun);
-console.log(square);
-
-const odd = nums.filter(function(element){
-  return element%2!=0;
-})
-console.log(odd);
-
+const hello2 = () => console.log(`Hello`);
+  
