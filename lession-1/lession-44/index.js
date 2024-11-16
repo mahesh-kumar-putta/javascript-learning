1dc0860669bf1b9014f8f83608559678809ffb7c
mahesh-kumar-putta <puttamahesh27@gmail.com>
2024-11-16 19:18:10 +0530

    Getters and Setters

---
+++ lession-1/lession-44/index.js
@@ -0,0 +1,53 @@
+// getter : special method that makes a property readable
+// setter : special method that makes a property writable
+
+// validate and modify a value when reading/writing a property
+
+class Reactange{
+
+  constructor(height, width){
+    this.height = height;
+    this.width = width;
+  }
+
+  // this will be called automatically
+  set height(newHeight){
+    if(newHeight>0){
+      // _ specifies a private variable
+      this._height = newHeight;
+    } else{
+      console.error(`Height cannot be negative`);
+    }
+  }
+
+  set width(newWidth){
+    if(newWidth>0){
+      // _ specifies a private variable
+      this._width = newWidth;
+    } else{
+      console.error(`Height cannot be negative`);
+    }
+  }
+
+  get height(){
+    return this._height;
+  }
+
+  get width(){
+    return this._width;
+  }
+
+  // here getter acts as function
+  get area(){
+    return this._height * this._width;
+  }
+
+}
+
+const rectangle1 = new Reactange(23,23);
+
+console.log(rectangle1.height);
+console.log(rectangle1.width);
+
+console.log(rectangle1.area);
+
